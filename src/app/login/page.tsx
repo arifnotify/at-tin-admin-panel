3f38668202c0e2bed4fb993fcf02310a34750583
arifnotify <arifnotify@gmail.com>
2026-05-28 21:39:00 +0300

    first 3

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,88 +4,50 @@ import api from "@/src/services/api";
 import { useState } from "react";
 
 export default function LoginPage() {
-  const [email, setEmail] =
-    useState("");
-
-  const [password, setPassword] =
-    useState("");
-
-  const [loading, setLoading] =
-    useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      setLoading(true);
-
-      const res = await api.post(
-        "/api/admin/login",
-        {
-          email,
-          password,
-        }
-      );
+      const res = await api.post("api/admin/login", {
+        email,
+        password,
+      });
 
       console.log(res.data);
 
-      // save token
-      localStorage.setItem(
-        "token",
-        res.data.access_token
-      );
-
       alert("Login Success");
-
-      window.location.href =
-        "/dashboard";
-    } catch (error: any) {
-      console.log(error);
-
-      alert(
-        error?.response?.data?.message ||
-          "Login Failed"
-      );
-    } finally {
-      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      alert("Login Failed");
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-[400px] bg-white p-6 rounded-2xl shadow-lg">
-        <h1 className="text-3xl font-bold mb-6 text-center">
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-[400px] p-6 bg-white shadow rounded-xl">
+        <h1 className="text-2xl font-bold mb-4">
           Admin Login
         </h1>
 
         <input
-          type="email"
+          className="w-full p-2 border mb-3"
           placeholder="Email"
-          className="w-full border p-3 rounded-lg mb-4"
-          value={email}
-          onChange={(e) =>
-            setEmail(e.target.value)
-          }
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
-          type="password"
+          className="w-full p-2 border mb-3"
           placeholder="Password"
-          className="w-full border p-3 rounded-lg mb-4"
-          value={password}
-          onChange={(e) =>
-            setPassword(
-              e.target.value
-            )
-          }
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button
           onClick={handleLogin}
-          disabled={loading}
-          className="w-full bg-black text-white p-3 rounded-lg"
+          className="w-full bg-black text-white p-2 rounded"
         >
-          {loading
-            ? "Loading..."
-            : "Login"}
+          Login
         </button>
       </div>
     </div>
