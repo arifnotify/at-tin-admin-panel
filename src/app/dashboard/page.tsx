6082c7a8328f1f3c6dd75fb698af2d2215224b34
arifnotify <arifnotify@gmail.com>
2026-05-28 23:57:44 +0300

    first 7

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,51 +1,14 @@
-const cards = [
-  {
-    title: "Total Users",
-    value: 1200,
-  },
-
-  {
-    title: "Total Orders",
-    value: 530,
-  },
-
-  {
-    title: "Total Products",
-    value: 340,
-  },
-
-  {
-    title: "Revenue",
-    value: "$12,000",
-  },
-];
-
 export default function DashboardPage() {
   return (
     <div>
 
-      <h1 className="text-3xl font-bold mb-6">
+      <h1 className="text-3xl font-bold">
         Dashboard
       </h1>
 
-      <div className="grid grid-cols-4 gap-5">
-
-        {cards.map((card) => (
-          <div
-            key={card.title}
-            className="bg-white p-5 rounded-2xl shadow"
-          >
-            <h2 className="text-gray-500">
-              {card.title}
-            </h2>
-
-            <h1 className="text-3xl font-bold mt-2">
-              {card.value}
-            </h1>
-          </div>
-        ))}
-
-      </div>
+      <p className="mt-3 text-gray-600">
+        Welcome to Admin Dashboard
+      </p>
 
     </div>
   );
