edee2b135702be2d8a6183d97390e8147ba11d20
Arif <90087547+arifnotify@users.noreply.github.com>
2026-05-31 18:55:35 +0300

    Update order.service.ts

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,37 +1,10 @@
 import api from "./api";
 
-export const getOrders =
-  async () => {
-    const res =
-      await api.get(
-        "/orders"
-      );
+export const getOrders = () =>
+  api.get("/orders").then((res) => res.data);
 
-    return res.data;
-  };
+export const getOrder = (id: string) =>
+  api.get(`/orders/${id}`).then((res) => res.data);
 
-export const getSingleOrder =
-  async (id: string) => {
-    const res =
-      await api.get(
-        `/orders/${id}`
-      );
-
-    return res.data;
-  };
-
-export const updateOrderStatus =
-  async (
-    id: string,
-    orderStatus: string
-  ) => {
-    const res =
-      await api.patch(
-        `/orders/${id}/status`,
-        {
-          orderStatus,
-        }
-      );
-
-    return res.data;
-  };
+export const updateStatus = (id: string, data: any) =>
+  api.patch(`/orders/${id}/status`, data);
